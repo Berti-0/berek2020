0e6f1169ede58cc9322fc0211753d0036ae86421
Borda Bertram <bertram.borda04@gmail.com>
2024-11-19 12:51:16 +0100

    Add project files.

---
+++ Dolgozo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace berek2020
+{
+    internal class Dolgozo
+    {
+        public string Nev { get; set; }
+        public bool Nem { get; set; }
+        public string Reszleg { get; set; }
+        public int Belepes { get; set; }
+        public int Ber { get; set; }
+
+        public Dolgozo(string sor)
+        {
+            string[] d = sor.Split(';');
+            Nev = d[0];
+            Nem = d[1] == "férfi";
+            Reszleg = d[2];
+            Belepes = int.Parse(d[3]);
+            Ber = int.Parse(d[4]);
+        }
+
+        public override string ToString()
+        {
+            return $"Név: {Nev}\nNem: {Nem}\nRészleg: {Reszleg}\nBelépés: {Belepes}\nBér: {Ber}";
+        }
+    }
+
+  
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using berek2020;
+using System.Text;
+List<Dolgozo> Dolgozok = [];
+
+using StreamReader sr = new(@"../../../src/berek2020.txt", Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream)
+{
+
+    Dolgozok.Add(new(sr.ReadLine()));
+}
+
+
+Console.WriteLine($"1. feladat\n" +
+    $"Dolgozó adatok száma: {Dolgozok.Count}\n");
+
+Console.WriteLine($"2. feladat\n" +
+    $"Bér átlag 2020: {Math.Round(Dolgozok.Average(d => d.Ber) / 1000,1)} ezer forint.\n");
+
+Console.Write($"3.feladat\n" +
+    $"Részleg neve: ");
+string reszleg = Console.ReadLine();
+if (Dolgozok.Select(d=> d.Reszleg).Contains(reszleg)) Console.WriteLine($"{Dolgozok.MaxBy(d=> d.Ber)}\n");
+else Console.WriteLine("A megadott részleg nem létezik a cégnél!\n");
+
+Console.WriteLine("4. feladat\n" +
+    "Az egyes részlegeken dolgozók száma:");
+var stat = Dolgozok.GroupBy(d => d.Reszleg).Select(d => new {Reszleg = d.Key, Letszam = d.Count()});
+foreach (var item in stat)
+{
+    Console.WriteLine($"{item.Reszleg} - {item.Letszam}");
+}
+
+Console.ReadKey();
